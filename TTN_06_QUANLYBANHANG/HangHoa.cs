cab0887b43812e8dd9e58e82533ce47e9f00e787
muoimuoisokiu <muoimuoisokiu@gmail.com>
2019-08-29 22:13:25 +0700

    Update HangHoa.cs

--- TTN_06_QUANLYBANHANG/HangHoa.cs
+++ TTN_06_QUANLYBANHANG/HangHoa.cs
@@ -17,9 +17,6 @@ namespace QuanLy
 		{
 
 		}
-		public string MaHH { get => maHH; set => maHH = value; }
-		public string TenHH { get => tenHH; set => tenHH = value; }
-		public DateTime NgayNhap { get => ngayNhap; set => ngayNhap = value; }
-		public DateTime NgayHetHan { get => ngayHetHan; set => ngayHetHan = value; }
+	
 	}
 }
